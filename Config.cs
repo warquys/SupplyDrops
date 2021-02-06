57704a47e6075ee29722c9f5fcc3f2f39d6215c4
TheVoidNebula <75329526+TheVoidNebula@users.noreply.github.com>
2021-02-06 22:28:09 +0100

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -6,34 +6,34 @@ namespace SupplyDrops
 {
     public class Config : AbstractConfigSection
     {
-        public bool IsEnabled = true;
+        public bool IsEnabled { get; set; } = true;
 
-        public int MinPlayersForSupply = 4;
+        public int MinPlayersForSupply { get; set; } = 4;
 
-        public bool IsOnlyHelicopter = true;
+        public bool IsOnlyHelicopter { get; set; } = true;
 
-        public int CiCarChance = 50;
+        public int CiCarChance { get; set; } = 50;
 
-        public List<SerializedItem> Items = new List<SerializedItem>()
+        public List<SerializedItem> Items { get; set; } = new List<SerializedItem>()
         {
             new SerializedItem((int) ItemType.Medkit, 0, 0, 0, 0, Vector3.one),
             new SerializedItem((int) ItemType.Medkit, 0, 0, 0, 0, Vector3.one),
             new SerializedItem((int) ItemType.Radio, 0, 0, 0, 0, Vector3.one)
         };
 
-        public float SupplyIntervall = 300;
+        public float SupplyIntervall { get; set; } = 300;
 
-        public bool DoBroadcast = true;
+        public bool DoBroadcast { get; set; } = true;
 
-        public ushort BroadcastDuration = 15;
+        public ushort BroadcastDuration { get; set; } = 15;
 
-        public bool DoCassieAnnouncement = true;
+        public bool DoCassieAnnouncement { get; set; } = true;
 
-        public string BroadcastMessageCI = "<b>A Supply drop has arrived via <color=#2EB800>CI Car</color></b>";
+        public string BroadcastMessageCI { get; set; } = "<b>A Supply drop has arrived via <color{ get; set; } =#2EB800>CI Car</color></b>";
 
-        public string BroadcastMessageMTF = "<b>A Supply drop has arrived via <color=#1F22C7>NTF Helicopter</color>!</b>";
+        public string BroadcastMessageMTF { get; set; } = "<b>A Supply drop has arrived via <color{ get; set; } =#1F22C7>NTF Helicopter</color>!</b>";
 
-        public string CassieAnnouncement = "Supply has entered the facility";
+        public string CassieAnnouncement { get; set; } = "Supply has entered the facility";
     }
 
 }
